6b85a6dc5111739f8315384e1a6c840c84409138
Anuj Soni <41353742+sonianuj287@users.noreply.github.com>
2020-06-22 12:45:50 +0530

    Create CountingBits-LeetCode-AnujSoni.cpp

---
+++ CountingBits-LeetCode-AnujSoni.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> res;
+        for(int i=0;i<=num;i++){
+            res.push_back(__builtin_popcount(i));
+        }
+        return res;
+    }
+};
