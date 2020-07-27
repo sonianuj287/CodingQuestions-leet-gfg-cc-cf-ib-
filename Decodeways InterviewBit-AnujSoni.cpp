a9c034f84474ce42395c00119d7a50af3b62a026
Anuj Soni <41353742+sonianuj287@users.noreply.github.com>
2020-07-27 19:57:41 +0530

    Create Decodeways InterviewBit-AnujSoni.cpp

---
+++ Decodeways InterviewBit-AnujSoni.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public: 
+    
+    int numDecodings(string A) {
+        int N=A.length();
+        int dp[N+1];
+        for(int i=0; i<=N; i++){
+            dp[i]=0;
+        }
+        dp[0] = 1;
+        dp[1] = A[0]=='0' ? 0 : 1;
+        for(int i=2; i<=N; i++){
+            int oneDigit=stoi(A.substr(i-1,1));
+            int twoDigit=stoi(A.substr(i-2,2));
+            if(oneDigit>=1){
+                dp[i] += dp[i-1];
+            }
+            if(twoDigit>=10 && twoDigit<=26){
+                dp[i] += dp[i-2];
+            }
+        }
+        return dp[N];
+    }
+};
