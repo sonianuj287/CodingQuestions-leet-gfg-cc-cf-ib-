5c7dd53250a1431a550b584ecbc9c747b97275e3
Anuj Soni <41353742+sonianuj287@users.noreply.github.com>
2020-08-15 09:53:02 +0530

    Update Max Product Subarray-InterviewBit-AnujSoni.cpp

--- Max Product Subarray-InterviewBit-AnujSoni.cpp
+++ Max Product Subarray-InterviewBit-AnujSoni.cpp
@@ -1,17 +1,15 @@
-class Solution {
-public:
-    int maxProduct(vector<int>& nums) {
-        int cMax = 1;
-        int cMin = 1;
-        int max_so_far = INT_MIN;
-        int n = nums.size();
-        for(int i=0;i<n;i++){
-            int pMax = nums[i]*cMax;
-            int pMin = nums[i]*cMin;
-            cMax = max({nums[i],pMax,pMin});
-            cMin = min({nums[i],pMax,pMin});
-            max_so_far = max(max_so_far,cMax);
-        }
-        return max_so_far;
+int Solution::maxProduct(const vector<int> &A) {
+    int n = A.size();
+    int dp1[n];
+    int dp2[n];
+    dp1[0] = A[0];
+    dp2[0] = A[0];
+    for(int i=1;i<n;i++){
+        dp1[i] = max(A[i],max(A[i]*dp1[i-1],A[i]*dp2[i-1]));
+        dp2[i] = min(A[i],min(A[i]*dp1[i-1],A[i]*dp2[i-1]));
     }
-};
+    int ans = INT_MIN;
+    for(int i=0;i<n;i++)
+        ans = max(ans,dp1[i]);
+    return ans;
+}
